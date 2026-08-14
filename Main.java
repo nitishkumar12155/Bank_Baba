505a6d42baeb25cfd5b63aaeb916d41fae18d674
unknown <nitishkumarbal42@gmail.com>
2026-08-14 15:04:16 +0530

    changes

--- Main.java
+++ Main.java
@@ -159,6 +159,10 @@ class Account extends Branch {
         return gender;
     }
 
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
     public String getMobile() {
         return mobile;
     }
@@ -184,9 +188,15 @@ class Account extends Branch {
     public String getAadhar() {
         return aadhar;
     }
+    public void setAadhar(String aadhar) {
+        this.aadhar = aadhar;
+    }
 
     public String getPan() {
         return pan;
+    }
+    public void setPAN(String pan) {
+        this.pan = pan;
     }
 
     public double getBalance() {
@@ -435,21 +445,26 @@ class Account extends Branch {
     }
 
    public void modifyAccount() {
-    System.out.print("Enter Account Number : ");
-    int accNo = sc.nextInt();
-    sc.nextLine();
-    Account acc = searchAccount(accNo);
-    if (acc == null) {
-        System.out.println("Account Not Found.");
-        return;
-    }
-    System.out.println("\n Modify Account Details ");
+    try {
+        System.out.print("Enter Account Number : ");
+        int accNo = sc.nextInt();
+        sc.nextLine();
+        Account acc = searchAccount(accNo);
+        if (acc == null) {
+            System.out.println("Account Not Found.");
+            return;
+        }
+        System.out.println("\n Modify Account Details ");
     System.out.println("1. Modify Name");
     System.out.println("2. Modify Mobile");
     System.out.println("3. Modify Email");
     System.out.println("4. Modify Address");
-    System.out.println("5. Modify All");
-    System.out.println("6. Exit");
+    System.out.println("5. Modify Age");
+    System.out.println("6. Modify Gender");
+    System.out.println("7. Modify Aadhar number");
+    System.out.println("8. Modify PAN");
+    System.out.println("9. Modify All");
+    System.out.println("10. Exit");
 
     System.out.print("Enter Your Choice : ");
     int choice = sc.nextInt();
@@ -464,14 +479,36 @@ class Account extends Branch {
             break;
 
         case 2:
-            System.out.print("Enter New Mobile : ");
-            acc.setMobile(sc.nextLine());
+            String number = "0";
+            while (true) {
+                System.out.print("Enter New Mobile Number: ");
+                number = sc.nextLine();
+                number = number.replace("+91", "").replace(" ", "");
+                if (number.length() == 10 && number.matches("[0-9]+")) {
+                    System.out.println("Valid Mobile Number");
+                    break;
+                } else {
+                    System.out.println("Invalid Mobile Number. Please enter a valid 10-digit mobile number.");
+                }
+            }
+            acc.setMobile(number);
             System.out.println("Mobile Updated Successfully.");
             break;
 
         case 3:
-            System.out.print("Enter New Email : ");
-            acc.setEmail(sc.nextLine());
+            
+        String Email;
+            while (true) {
+                System.out.print("Enter New Email: ");
+                Email = sc.nextLine();
+                if (Email.contains("@") && Email.endsWith(".com")) {
+                    System.out.println("Valid Email ");
+                    break;
+                } else {
+                    System.out.println("Invalid Email. Please enter a valid email address.");
+                }
+            }
+            acc.setEmail(Email);
             System.out.println("Email Updated Successfully.");
             break;
 
@@ -482,32 +519,166 @@ class Account extends Branch {
             break;
 
         case 5:
+            System.out.print("Enter New Age : ");
+            int age = sc.nextInt();
+            sc.nextLine();
+            if (age < 18)
+                throw new UnderAgeException();
+            if (age > 60)
+                throw new OverAgeException();
+            acc.setAge(age);
+            System.out.println("Age Updated Successfully.");
+            break;
+
+        case 6:
+            System.out.print("Enter Gender (Male/Female/Other) : ");
+            String gender = sc.nextLine();
+            if(!(gender.equalsIgnoreCase("Male") ||
+                gender.equalsIgnoreCase("Female") ||
+                gender.equalsIgnoreCase("Other")||
+                gender.equalsIgnoreCase("M") ||
+                gender.equalsIgnoreCase("F") ||
+                gender.equalsIgnoreCase("O")))
+                {
+                    throw new Exception();
+                }
+                acc.setGender(gender);
+        case 7:
+            long aadhar;
+            while (true) {
+                System.out.print("Enter Aadhar Number: ");
+                aadhar = sc.nextLong();
+                if (String.valueOf(aadhar).length() == 12) {
+                    System.out.println("Valid Aadhar Number");
+                    break;
+                } else {
+                    System.out.println("Invalid Aadhar Number. Please enter a valid 12-digit Aadhar number.");
+                }
+            }
+            acc.setAadhar(String.valueOf(aadhar));
+            System.out.println("Aadhar Updated Successfully.");
+            break;
+
+        case 8:
+            String pan;
+            while (true) {
+                System.out.print("Enter PAN Number: ");
+                pan = sc.next();
+                if (pan.length() == 10 && pan.matches("[A-Z]{5}[0-9]{4}[A-Z]")) {
+                    System.out.println("Valid PAN Number");
+                    break;
+                } else {
+                    System.out.println("Invalid PAN Number. Please enter a valid PAN number.");
+                }
+            }
+            acc.setPAN(pan);
+            System.out.println("PAN Updated Successfully.");
+            break;
+
+        case 9:
             System.out.print("Enter New Name : ");
             acc.setName(sc.nextLine());
 
             System.out.print("Enter New Mobile : ");
-            acc.setMobile(sc.nextLine());
+            number = "0";
+            while (true) {
+                number = sc.nextLine();
+                number = number.replace("+91", "").replace(" ", "");
+                if (number.length() == 10 && number.matches("[0-9]+")) {
+                    System.out.println("Valid Mobile Number");
+                    break;
+                } else {
+                    System.out.println("Invalid Mobile Number. Please enter a valid 10-digit mobile number.");
+                }
+            }
+            acc.setMobile(number);
 
-            System.out.print("Enter New Email : ");
-            acc.setEmail(sc.nextLine());
+            Email = "";
+            while (true) {
+                System.out.print("Enter New Email : ");
+                Email = sc.nextLine();
+                if (Email.contains("@") && Email.endsWith(".com")) {
+                    System.out.println("Valid Email ");
+                    break;
+                } else {
+                    System.out.println("Invalid Email. Please enter a valid email address.");
+                }
+            }
+            acc.setEmail(Email);
 
             System.out.print("Enter New Address : ");
             acc.setAddress(sc.nextLine());
 
+            System.out.print("Enter New Age : ");
+            age = sc.nextInt();
+            sc.nextLine();
+            if (age < 18)
+                throw new UnderAgeException();
+            if (age > 60)
+                throw new OverAgeException();
+            acc.setAge(age);
+            System.out.println("Age Updated Successfully.");
+
+            System.out.print("Enter Gender (Male/Female/Other) : ");
+            gender = sc.nextLine();
+            if(!(gender.equalsIgnoreCase("Male") ||
+                gender.equalsIgnoreCase("Female") ||
+                gender.equalsIgnoreCase("Other")||
+                gender.equalsIgnoreCase("M") ||
+                gender.equalsIgnoreCase("F") ||
+                gender.equalsIgnoreCase("O")))
+                {
+                    throw new Exception();
+                }
+                acc.setGender(gender);
+
+            aadhar = 0;
+            while (true) {
+                System.out.print("Enter Aadhar Number: ");
+                aadhar = sc.nextLong();
+                if (String.valueOf(aadhar).length() == 12) {
+                    System.out.println("Valid Aadhar Number");
+                    break;
+                } else {
+                    System.out.println("Invalid Aadhar Number. Please enter a valid 12-digit Aadhar number.");
+                }
+            }
+            acc.setAadhar(String.valueOf(aadhar));
+            System.out.println("Aadhar Updated Successfully.");
+
+            pan = "";
+            while (true) {
+                System.out.print("Enter PAN Number: ");
+                pan = sc.next();
+                if (pan.length() == 10 && pan.matches("[A-Z]{5}[0-9]{4}[A-Z]")) {
+                    System.out.println("Valid PAN Number");
+                    break;
+                } else {
+                    System.out.println("Invalid PAN Number. Please enter a valid PAN number.");
+                }
+            }
+            acc.setPAN(pan);
+
             System.out.println("Account Updated Successfully.");
             break;
 
-        case 6:
-            System.out.println("Modification Cancelled.");
-            break;
 
         default:
             System.out.println("Invalid Choice.");
     }
+    }
+    catch (UnderAgeException e) {
+        System.out.println(e.getMessage());
+    }
+    catch (OverAgeException e) {
+        System.out.println(e.getMessage());
+    }
+    catch (Exception e) {
+        System.out.println("Invalid Input.");
+    }
 }
 
     public class Main {
-
         public static void main(String[] args) {
             
             Scanner sc = new Scanner(System.in);
@@ -559,3 +730,4 @@ class Account extends Branch {
         }
     }
 }
+
